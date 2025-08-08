9f813263e99b9172b586a3caf9261a1c81526e2e
RANAJOY NAG <iemrono2022@gmail.com>
2025-08-08 20:18:19 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="o

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { useTheme } from "next-themes"
-import { Check, Moon, Sun } from "lucide-react"
+import { Check, Palette } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -16,7 +16,7 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ThemeSwitcher() {
-  const { setTheme, theme, resolvedTheme } = useTheme()
+  const { setTheme, theme } = useTheme()
 
   const themes = [
     { name: "light", label: "Light" },
@@ -34,8 +34,7 @@ export function ThemeSwitcher() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:hidden" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Palette className="h-[1.2rem] w-[1.2rem]" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
