72ddafebffb42a317aeeec203b97262cd09daa79
RANAJOY NAG <iemrono2022@gmail.com>
2025-08-10 07:06:07 +0000

    if i open in mobile the layout does not changes

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -194,8 +194,9 @@ const Sidebar = React.forwardRef<
 
     if (isMobile) {
       return (
-        <Sheet open={openMobile} onOpenChange={setOpenMobile} {...props}>
+        <Sheet open={openMobile} onOpenChange={setOpenMobile}>
           <SheetContent
+            side={side}
             data-sidebar="sidebar"
             data-mobile="true"
             className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
@@ -204,7 +205,6 @@ const Sidebar = React.forwardRef<
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
               } as React.CSSProperties
             }
-            side={side}
           >
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
